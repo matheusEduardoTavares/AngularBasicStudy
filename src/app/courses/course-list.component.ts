9dfe3d05817a72ee3f368635443e4e73bc79c880
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-11-29 02:28:58 -0300

    onInit, ngFor, componentes, classes para interface, importacao do bootstrap

---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
+
+@Component({
+    selector: 'app-course-list',
+    // template: '<h2> Course List </h2>',
+    templateUrl: 'course-list.component.html'
+})
+///Implementamos métodos do ciclo de vida
+export class CourseListComponent implements OnInit {
+  courses: Course[] = [];
+
+  ngOnInit(): void {
+    this.courses.push(
+      {
+        id: 1,
+        name: 'Angular Forms',
+        imageUrl: '',
+        price: 99.99,
+        code: 'XPS-8796',
+        duration: 120,
+        rating: 5.4,
+        releaseDate: 'November, 2, 2019'
+      },
+      {
+        id: 2,
+        name: 'Angular HTTP',
+        imageUrl: '',
+        price: 45.99,
+        code: 'LML-1094',
+        duration: 80,
+        rating: 4,
+        releaseDate: 'December, 4, 2019'
+      },
+    );
+  }
+}
