26dc19cebe9ec79c460c4b7f8359c83f2c4ed1d2
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-12-04 13:58:29 -0300

    Variaveis de template, ngClass, validacoes de formulario, (click)

--- src/app/courses/courses.info.component.ts
+++ src/app/courses/courses.info.component.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { Course } from "./course";
+import { CourseService } from "./course.service";
 
 @Component({
   templateUrl: 'courses.info.component.html',
 })
 export class CoursesInfoComponent implements OnInit{
-  courseId: number;
+  course: Course;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private courseService: CourseService,
+  ) {}
 
   ngOnInit(): void {
     // this.courseId = Number(this.route.snapshot.paramMap.get('id')!);
-    this.courseId = +this.route.snapshot.paramMap.get('id')!;
+    this.course = this.courseService.retrieveById(
+      +this.route.snapshot.paramMap.get('id')!
+    );
+  }
+
+  save(): void {
+    this.courseService.save(this.course);
   }
 
 }
